2551eade03010c86aaa29e1caab4f7d886ba42dd
Tomer Klein <tomer.klein@gmail.com>
2021-02-15 17:17:35 +0200

    Added imports and default method

--- arpspoof.py
+++ arpspoof.py
@@ -1,10 +1,21 @@
 from flask import Flask, request, make_response, render_template, url_for, g, send_from_directory, jsonify
 from flask_restful import Resource, Api
+import os, json, subprocess, time, broadlink, argparse, datetime, re
+from os import environ, path
+from json import dumps
+from subprocess import call
+from loguru import logger
 
 
 app = Flask(__name__)
 api = Api(app)
 
+
+
+@app.route('/')
+def default():
+    logger.info("Number five is alive!")
+    return "Number five is alive!"
 
 
 
